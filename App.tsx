78ecbcd19b5eec3c2c7f49575d80dc3cee36eaf3
MJ Abadilla <mjmaix@gmail.com>
2022-09-10 21:03:39 +0800

    test lint-staged prettier

--- App.tsx
+++ App.tsx
@@ -3,11 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View 
-    style={styles.container}>
+    <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" 
-      />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent:"center",
+    justifyContent: "center",
   },
 });
