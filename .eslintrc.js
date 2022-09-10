6d4bdb1ff32882ea0d3342ed7cfc5162376769d8
MJ Abadilla <mjmaix@gmail.com>
2022-09-10 21:34:37 +0800

    enforce import soring with eslint import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,43 @@
 module.exports = {
-  extends: ["@react-native-community", "eslint-config-prettier"],
+  extends: [
+    "@react-native-community",
+    "eslint-config-prettier",
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  plugins: ["import"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        typescript: true,
+        alwaysTryTypes: true,
+        project: ".",
+      },
+    },
+  },
+  rules: {
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        "newlines-between": "always",
+        groups: ["builtin", "external", "parent", "sibling", "index"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+      },
+    ],
+  },
 };
